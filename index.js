7e964f6989e45a24e1194ea0bef8b5aa2f072c13
stevenjhomem <sjhomem@outlook.com>
2021-07-22 10:39:47 -0500

    Finished Task 5

--- index.js
+++ index.js
@@ -176,8 +176,25 @@ class Instructor extends Lambdasian {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
-   
+class Student extends Lambdasian {
+   constructor(studentAttributes){
+     super(studentAttributes);
+     this.previousBackground = studentAttributes.previousBackground;
+     this.className = studentAttributes.className;
+     this.favSubjects = studentAttributes.favSubjects;
+   }
+
+   listSubjects(){
+     return `Loving ${this.favSubjects.toString()}!`;
+   }
+
+   PRAssignment(subject){
+     return `${this.name} has submitted a PR for ${subject}`;
+   }
+
+   sprintChallenge(subject){
+     return `${this.name} has begun sprint challenge on ${subject}`;
+   }
 }
 
 /*
@@ -193,7 +210,7 @@ class Student {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
+class ProjectManager extends Instructor {
    
 }
 /*
